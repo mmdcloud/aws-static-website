a16967c4dad94de0903f132d01162360e32d1fd8
mmdcloud <148881191+mmdcloud@users.noreply.github.com>
2024-08-16 15:50:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,3 +110,36 @@ resource "aws_s3_bucket_policy" "append_s3_bucket_policy" {
     ]
   })
 }
+
+# Route 53 Zone Configuration 
+resource "aws_route53_zone" "route53_zone" {
+  name          = var.domain_name
+  force_destroy = true
+}
+
+resource "aws_route53_health_check" "health_check" {
+  fqdn              = module.singapore-resources.lb-ip
+  port              = 80
+  type              = "HTTP"
+  resource_path     = "/"
+  failure_threshold = "5"
+  request_interval  = "30"
+
+  tags = {
+    Name = "health-check"
+  }
+}
+
+# Route 53 Record Configuration 
+resource "aws_route53_record" "route53_record" {
+  zone_id         = aws_route53_zone.shiftme_route53_zone.zone_id
+  set_identifier  = "mumbai"
+  name            = var.subdomain_name
+  type            = "CNAME"
+  health_check_id = aws_route53_health_check.mumbai_health_check.id
+  failover_routing_policy {
+    type = "PRIMARY"
+  }
+  ttl     = 300
+  records = ["${module.mumbai-resources.lb-ip}"]
+}
